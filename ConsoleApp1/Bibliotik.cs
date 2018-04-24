ca0710e35b43bfe148ea286dbc0ff900b263b0e8
Mdalol <stas-limon99@mail.ru>
2018-04-24 11:20:20 +0300

    War and Peace

---
+++ ConsoleApp1/Bibliotik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    ///     Описываем класс Изделие
+    /// </summary>
+    class Bibliotik
+    {
+        public string Name { get; set; }
+        public string Adres { get; set; }
+        public int God { get; set; }
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Bibliotik()
+        {
+        }
+        /// <summary>
+        /// Ввод объектов класса с клавиатуры
+        /// </summary>
+        public void Input()
+        {
+            Name = Console.ReadLine();
+            Adres = Console.ReadLine();
+            God = int.Parse(Console.ReadLine());
+        }
+    }
+}
